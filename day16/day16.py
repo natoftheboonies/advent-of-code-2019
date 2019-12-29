02d86c5c005065a056fff7b5ad0daaa7bbd7e803
Nat Peterson <nat.peterson@workday.com>
2019-12-29 19:06:18 +0100

    day 16, part 2 after hint

--- day16/day16.py
+++ day16/day16.py
@@ -37,9 +37,19 @@ assert calc_rounds('80871224585914546619083218645595',100)[:8]=='24176176'
 assert calc_rounds('19617804207202209144916044189917',100)[:8]=='73745418'
 assert calc_rounds('69317163492948606335995924319873',100)[:8]=='52432133'
 
-#print('#1',calc_rounds(input,100)[:8])
-##1 74369033
+print('#1',calc_rounds(input,100)[:8])
+#1 74369033
 #[Finished in 12.7s]
 
-offset = input[:7]
-print(offset)
+offset = int(input[:7])
+#print(offset)
+
+signal = list(map(int,list(input)))
+signal = (signal*10000)[offset:]
+
+for _ in range(100):
+	for i in range(len(signal)-1, 0, -1): # go backwards, sez reddit
+		signal[i-1] = (signal[i-1] + signal[i]) % 10
+print('#2',''.join(map(str, signal[:8])))
+#2 19903864
+#[Finished in 16.0s]
